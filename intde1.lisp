96d33357e85c572eb8529ea8dc3304b63f76b7a6
Dimiter Prodanov (imec) <prodanov@imec.be>
2021-01-22 00:58:42 +0100

    update

--- intde1.lisp
+++ intde1.lisp
@@ -1,3 +1,20 @@
+;	Courtesy to Prof. Takuya Ooura
+; 	Library for computation of speial functions
+; 	and numerical integration
+; 	
+; The code is based in mostly on
+; http://www.kurims.kyoto-u.ac.jp/~ooura/intde.html
+;  for the tanh-sinh (double exponential) method
+;  
+; Bibliography
+; M.Mori, Developments in the double exponential formula for numerical integration, Proceedings of the International Congress of Mathematicians, Kyoto 1990, 1991, Springer-Verlag, 1585-1594.
+; H.Takahasi and M.Mori, Double exponential formulas for numerical integration, Pub. RIMS Kyoto Univ. 9, 1974, 721-741
+; T.Ooura and M.Mori, The Double exponential formula for oscillatory functions over the half infinite interval, Journal of Computational and Applied Mathematics 38, 1991, 353-360
+; M.Mori and T.Ooura, Double exponential formulas for Fourier type integrals with a divergent integrand, Contributions in Numerical Mathematics, ed. R.P.Agarwal, World Scientific Series in Applicable Analysis, 2, 1993, 301-308
+; H.Toda and H.Ono, Some remarks for efficient usage of the double exponential formulas(in Japanese), Kokyuroku, RIMS, Kyoto Univ. 339, 1978, 74-109.
+ 
+
+
 ;;; Compiled by f2cl version:
 ;;; ("f2cl1.l,v 95098eb54f13 2013/04/01 00:45:16 toy $"
 ;;;  "f2cl2.l,v 95098eb54f13 2013/04/01 00:45:16 toy $"
@@ -21,7 +38,8 @@
 
 (in-package :quadde)
 
-
+; intde computes the  integral of f(x) over (a,b)
+;
 (defun intde (f a b eps err)
   (declare (type (double-float) err eps b a))
   (prog ((pi2 0.0d0) (epsln 0.0d0) (epsh 0.0d0) (h0 0.0d0) (ehp 0.0d0)
@@ -125,7 +143,10 @@
 
 (in-package :quadde)
 
-
+;intdei computes the integral of f(x) over (a,infinity), 
+;	            f(x) has no oscillatory factor!
+;	f(x) needs to be analytic over (a,infinity).
+	
 (defun intdei (f a eps err)
   (declare (type (double-float) err eps a))
   (prog ((pi4 0.0d0) (epsln 0.0d0) (epsh 0.0d0) (h0 0.0d0) (ehp 0.0d0)
@@ -225,7 +246,10 @@
 
 (in-package :quadde)
 
-
+;intdeo computes the integral of f(x) over (a, infinity), 
+;            f(x) has an oscillatory factor :
+;            f(x) = g(x) * sin(omega * x + theta) as x -> infinity.
+			
 (defun intdeo (f a omega eps err)
   (declare (type (double-float) err eps omega a))
   (prog ((pi4 0.0d0) (epsln 0.0d0) (epsh 0.0d0) (frq4 0.0d0) (per2 0.0d0)
