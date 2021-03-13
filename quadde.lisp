9b892d527a6e824ab5a34cfeeb7952987cf4ec66
Dimiter Prodanov (imec) <prodanov@imec.be>
2021-03-13 22:38:09 +0100

    version1.1

--- quadde.lisp
+++ quadde.lisp
@@ -1,6 +1,9 @@
 ;;; Maxima interface to double exponential integration
 ;;  
 ; (c) Dimiter Prodanov
+; version 1.1 13 March 2021
+; - change in condition checking
+; - compatibilty with Maxima 44
 ; version 1.0 23 Jan 2021
 ; updates in quadde_argument_check
 ; input from Michel Talon
@@ -23,10 +26,6 @@
 ;; error checking similar to that done by $defint
 ;; we don't really need to simplify 
 (defun quadde_argument_check (expr var ll ul) 
-  ;(setq expr (ratdisrep expr))
-  ;(setq var (ratdisrep var))
-  ;(setq ll (ratdisrep ll))
-  ;(setq ul (ratdisrep ul))
   (cond (($constantp var)
 	 (merror "Variable of integration not a variable: ~M"
 		 var)))
@@ -41,37 +40,13 @@
 		 var expr)))		  
    (cond ((or (among var ul) (among var ll))
 	  (merror "Terminal contains variable of integration: ~M" var)))
-   (cond ((not (and ($numberp ul) ($numberp ll)))
+   (cond ((not (and ($numberp (coerce-float ul)) ($numberp (coerce-float ll))))
 	  (merror "Terminal not a number ~M , ~M" ll ul)))
 )  
 
 
-; (defun quad-intde (fun var a b &key
-		 ; (epsrel 1e-8)
-		 ; (epsabs 0.0))
-  ; (quadde_argument_check fun var a b) 
-  
-	; (handler-case
-	; (multiple-value-bind ( i err )
-		; (quadde::intde (float-integrand-or-lose '$quad_intde fun var )
-			 ; (float-or-lose a)
-			 ; (float-or-lose b)
-			 ; (float-or-lose epsrel)
-			 ; (float-or-lose epsabs)
-		; )
 
-	  ; (list '(mlist) i err))
-	  ; (error (e)
-	; (when *debug-quadde*
-	  ; (format t "~S" e))
-	; `(($quad_intde) ,fun ,var ,a ,b 
-	  ; ((mequal) $epsrel ,epsrel)
-	  ; ((mequal) $epsabs ,epsabs)
-	 ; )
-	  ; ))
-; )
-
-(defun quad-intde (fun var a b &key
+(defmfun $quad_intde (fun var a b &key
 		 (epsrel 1e-8)
 		 (epsabs 0.0))
   (quadde_argument_check fun var a b) 
@@ -101,31 +76,7 @@
 )
  
 
-; (defun quad-intdei (fun var a  &key
-		 ; (epsrel 1e-8)
-		 ; (epsabs 0.0))
-  ; (quadde_argument_check fun var a a) 
-  
-	; (handler-case
-	; (multiple-value-bind ( i err )
-		; (quadde::intdei (float-integrand-or-lose '$quad_intdei fun var)
-			 ; (float-or-lose a)
-			 ; (float-or-lose epsrel)
-			 ; (float-or-lose epsabs)
-		; )
-
-	  ; (list '(mlist) i err))
-	  ; (error (e)
-	; (when *debug-quadde*
-	  ; (format t "~S" e))
-	; `(($quad_intdei) ,fun ,var ,a 
-	  ; ((mequal) $epsrel ,epsrel)
-	  ; ((mequal) $epsabs ,epsabs)
-	 ; )
-	  ; ))
-; )
-
-(defun quad-intdei (fun var a  &key
+(defmfun $quad_intdei (fun var a  &key
 		 (epsrel 1e-8)
 		 (epsabs 0.0))
   (quadde_argument_check fun var a a) 
@@ -152,33 +103,9 @@
 	  ))
 	 )
 )
-
-; (defun quad-intdeo (fun var a omega &key
-		 ; (epsrel 1e-8)
-		 ; (epsabs 0.0))
-  ; (quadde_argument_check fun var a omega) 
   
-	; (handler-case
-	; (multiple-value-bind ( i err )
-		; (quadde::intdeo (float-integrand-or-lose '$quad_intdeo fun var)
-			 ; (float-or-lose a)
-			 ; (float-or-lose omega)
-			 ; (float-or-lose epsrel)
-			 ; (float-or-lose epsabs)
-		; )
 
-	  ; (list '(mlist) i err))
-	  ; (error (e)
-	; (when *debug-quadde*
-	  ; (format t "~S" e))
-	; `(($quad_intdeo) ,fun ,var ,a ,omega 
-	  ; ((mequal) $epsrel ,epsrel)
-	  ; ((mequal) $epsabs ,epsabs)
-	 ; )
-	  ; ))
-; )	  
-
-(defun quad-intdeo (fun var a omega &key
+(defmfun $quad_intdeo (fun var a omega &key
 		 (epsrel 1e-8)
 		 (epsabs 0.0))
   (quadde_argument_check fun var a omega) 
@@ -207,15 +134,3 @@
 	 )
 )
 	  
-(macrolet
-    ((frob (mname iname args valid-keys)
-       `(defun-checked ,mname ((,@args) ,@valid-keys)
-	  ;; BIND EVIL SPECIAL VARIABLE *PLOT-REALPART* HERE ...
-	  (let ((*plot-realpart* nil))
-	    (declare (special *plot-realpart*))
-	    (apply ',iname ,@args keylist)))))
-  (frob $quad_intde quad-intde (fun var a b) ($epsrel $epsabs))
-  (frob $quad_intdei quad-intdei (fun var a ) ($epsrel  $epsabs))
-  (frob $quad_intdeo quad-intdeo (fun var a omega) ($epsrel  $epsabs))
-
- )
